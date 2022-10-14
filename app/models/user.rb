c7066da2653a1c7401aad7a9fb3c220b3bb05481
Brett Tanner <bretttanner171@gmail.com>
2022-10-14 13:57:56 +0900

    Check User validations work

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, :pwd, presence: true
+
+  validates :name, uniqueness: true
+  validates :name, length: {in: 3..20}
+  validates :name, format: {with: /[a-zA-Z0-9]+/, message: "must be only alphanumeric characters", multiline: true}
+
+  validates :pwd, length: {minimum: 8}
+  validates :pwd, confirmation: true
+  validates :pwd_confirmation, presence: true
+  validates :pwd, format: {with: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, message: "must have uppercase, lowercase and number/special character", multiline: true}
+end
