29d88a5645b4e7643110c99401534c9a590eda51
Brett Tanner <bretttanner171@gmail.com>
2022-10-15 11:42:22 +0900

    Create Comment model and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
-  validates :title, :body, presence: true
+  validates :title, :body, :user_id, presence: true
 
   validates :title, length: {maximum: 50}
 end
